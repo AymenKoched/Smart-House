6c339fc24767dfbd2c832e30069a579ca0c2a17c
Aymen Koched <aymenkoched@gmail.com>
2024-04-20 00:16:08 +0100

    fix: env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import process from 'process';
+import * as process from 'process';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
