c598fa82cd665964da7b5ebadddc0a3aa2d0315f
HabibTriki <mohamedhabib.triki@insat.ucar.tn>
2024-04-24 22:14:22 +0100

    add card code

---
+++ card code/cardcode.ino
@@ -0,0 +1,81 @@
+#include <WiFi.h> 
+#include <WebServer.h>
+#include <ArduinoJson.h> 
+const char* ssid = "elite2.4G1";
+const char* password = "eliterobotique5G@";
+String M,aux ;
+WebServer server(80);
+
+void handleData() {
+  if (server.hasArg("plain")) {
+    String jsonMessage = server.arg("plain"); 
+    // Serial.print("Received JSON message: ");
+    // Serial.println(jsonMessage);
+    StaticJsonDocument<200> doc; 
+    DeserializationError error = deserializeJson(doc, jsonMessage);
+    if (error) {
+      server.send(400, "application/json", "Error parsing JSON");
+    } 
+    else {
+      const char* messageValue = doc["message"];
+      M=String(messageValue);
+      server.send(200, "application/json",messageValue );
+    }
+  } 
+  else {
+    server.send(400, "text/plain", "Missing JSON message");
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  for(int i=0;i<34;i++){
+    if(!(6<=i<=11||i==0||i==1||i==3)){
+      pinMode(i,OUTPUT);
+    }
+  }
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting...");
+  }
+  Serial.println(WiFi.localIP());
+  server.on("/read_request", HTTP_POST, handleData); 
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+  if (M!=aux) {
+    String a = M;
+    if ( a[0]=='L'){
+      int N = a.substring(1,3).toInt();
+      String ch= a.substring(3,a.length());
+      if(ch=="OFF"){
+        digitalWrite(N, 0);
+      }
+      else if (ch=="ON"){
+        digitalWrite(N, 1);
+      }
+    }
+    else if ( a[0]=='W'){
+      int N1 = a.substring(1,3).toInt();// Up
+      int N2 = a.substring(3,5).toInt();// Down
+      String ch= a.substring(5,a.length());
+      if(ch=="UP"){
+        digitalWrite(N1, 0);
+        digitalWrite(N2, 1);
+      }
+      else if (ch=="DOWN"){
+        digitalWrite(N1, 1);
+        digitalWrite(N2, 0);
+      }
+      else if (ch=="STOP"){
+        digitalWrite(N1, 0);
+        digitalWrite(N2, 0);
+      } 
+    }
+    aux=M;
+  }
+}
